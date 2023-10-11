2dc4d0848c70658076a43cff4cbb0de825744929
4l1onGit <hassanali2562@gmail.com>
2023-10-11 23:54:02 +0100

    Add loading skeletons + Font fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,10 @@ import { Inter } from "next/font/google";
 import { Theme, ThemePanel } from "@radix-ui/themes";
 import NavBar from "./NavBar";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,8 +22,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.variable}>
+    <html lang="en" className={inter.variable}>
+      <body>
         <Theme accentColor="teal" radius="large">
           <NavBar />
           <main className="p-5">{children}</main>
