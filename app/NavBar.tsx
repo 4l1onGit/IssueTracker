f17f9216efb7ad4fa44acc1d03652203421e84af
4l1onGit <hassanali2562@gmail.com>
2023-10-11 19:13:19 +0100

    Style the navbar and active link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,20 +1,33 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { RiAlarmWarningFill } from "react-icons/ri";
+import classnames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
   return (
-    <nav className="flex space-x-6 borde-b mb-5 px-5 h-14 items-center">
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
         <RiAlarmWarningFill />
       </Link>
       <ul className="flex space-x-6">
         {links.map((link) => (
-          <Link key={link.href} href={link.href}>
+          <Link
+            key={link.href}
+            className={classnames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+            href={link.href}
+          >
             {link.label}
           </Link>
         ))}
