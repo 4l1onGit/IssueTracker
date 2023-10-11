dfe47c6552aa2b0118cd9d7d0803e4902878f669
4l1onGit <hassanali2562@gmail.com>
2023-10-12 00:53:16 +0100

    Refactor: Organise Imports

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button, Table } from "@radix-ui/themes";
-import Skeleton from "react-loading-skeleton";
+import { Skeleton } from "@/app/components";
 import IssueActions from "./IssueActions";
 
 const LoadingIssuesPage = () => {
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,6 @@
-import React from "react";
-import { Table } from "@radix-ui/themes";
-import Link from "../components/Link";
 import prisma from "@/prisma/client";
-import IssueStatusBadge from "../components/IssueStatusBadge";
+import { Table } from "@radix-ui/themes";
+import { IssueStatusBadge, Link } from "@/app/components";
 import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
@@ -26,10 +24,7 @@ const IssuesPage = async () => {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                <Link
-                  href={`/issues/${issue.id}`}
-                  children={issue.title}
-                ></Link>
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                 <div className="block md:hidden">
                   <IssueStatusBadge status={issue.status}></IssueStatusBadge>
                 </div>
